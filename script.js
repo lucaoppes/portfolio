7d1761c3fec52151672dcafbddf92c97b6833712
Luca <oppes.luca1@gmail.com>
2026-01-14 18:46:35 +0000

    some layout updates

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 function flip(e) {
   e.currentTarget.classList.toggle("click");
 }
+
+VANTA.TRUNK({
+  el: "html",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  backgroundColor: 0x2d2e3e,
+  color: 0xf74972,
+  scale: 1,
+  scaleMobile: 1,
+  spacing: 5,
+  chaos: 1.5,
+});
